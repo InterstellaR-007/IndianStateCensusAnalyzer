349cb406c0b57e8579327fb1d8734e85901563f2
InterstellaR-007 <intrstelr.007@gmail.com>
2020-10-28 20:42:59 +0530

    [Anuj] Updated Test Cases for both State Code and Census

--- CensusAnalyzerTest/UnitTest1.cs
+++ CensusAnalyzerTest/UnitTest1.cs
@@ -12,12 +12,13 @@ namespace CensusAnalyzerTest
         static string indianStateCensusFilePath = @"C:\Users\anujs\source\repos\IndianStateCensusAnalyzer\CensusAnalyzerTest\CSVFiles\IndiaStateCensusData.csv";
         static string wrongHeaderIndianCensusFilePath = @"C:\Users\anujs\source\repos\IndianStateCensusAnalyzer\CensusAnalyzerTest\CSVFiles\WrongIndiaStateCensusData.csv";
         static string delimiterIndianCensusFilePath = @"C:\Users\anujs\source\repos\IndianStateCensusAnalyzer\CensusAnalyzerTest\CSVFiles\DelimiterIndiaStateCensusData.csv";
-        static string wrongIndianStateCensusFilePath = @"C:\Users\anujs\source\repos\IndianStateCensusAnalyzer\CensusAnalyzerTest\CSVFiles\WrongIndiaStateCensusData.csv";
+        static string wrongIndianStateCensusFilePath = @"C:\Users\anujs\source\repos\IndianStateCensusAnalyzer\CensusAnalyzerTest\CSVFiles\WrongIndiaStateCensusDataFilePath.csv";
         static string wrongIndianStateCensusFileType = @"C:\Users\anujs\source\repos\IndianStateCensusAnalyzer\CensusAnalyzerTest\CSVFiles\IndiaStateCensusData.txt";
         static string indianStateCodeFilePath = @"C:\Users\anujs\source\repos\IndianStateCensusAnalyzer\CensusAnalyzerTest\CSVFiles\IndiaStateCode.csv";
         static string wrongIndianStateCodeFileType = @"C:\Users\anujs\source\repos\IndianStateCensusAnalyzer\CensusAnalyzerTest\CSVFiles\IndiaStateCode.txt";
         static string delimiterIndianStateCodeFilePath = @"C:\Users\anujs\source\repos\IndianStateCensusAnalyzer\CensusAnalyzerTest\CSVFiles\DelimiterIndiaStateCode.csv";
         static string wrongHeaderStateCodeFilePath = @"C:\Users\anujs\source\repos\IndianStateCensusAnalyzer\CensusAnalyzerTest\CSVFiles\WrongIndiaStateCode.csv";
+        static string wrongIndianStateCodeFilePath = @"C:\Users\anujs\source\repos\IndianStateCensusAnalyzer\CensusAnalyzerTest\CSVFiles\WrongIndiaStateCodeFilePath.txt";
 
         IndianStateCensusAnalyzer.POCO.CensusAnalyzer censusAnalyzer;
         Dictionary<string, CensusDTO> totalRecords;
@@ -35,12 +36,156 @@ namespace CensusAnalyzerTest
         [Test]
         public void GivenIndianCensusDataFile_WhenRead_ShouldReturnCensusDataCount()
         {
-            totalRecords = censusAnalyzer.LoadCensusData(Country.INDIA, indianStateCensusFilePath, indianStateCensusHeaders);
-            stateRecords = censusAnalyzer.LoadCensusData(Country.INDIA, indianStateCodeFilePath, indianStateCodeHeaders);
+            Dictionary<string, CensusDTO> indiaStateCensusTotalRecords = new Dictionary<string, CensusDTO>();
 
+            indiaStateCensusTotalRecords = censusAnalyzer.LoadCensusData(Country.INDIA, indianStateCensusFilePath, indianStateCensusHeaders);
+         
+            Assert.AreEqual(29, indiaStateCensusTotalRecords.Count);
+            
+        }
+        [Test]
+        public void GivenIndianStateCodeDataFile_WhenRead_ShouldReturnCodeDataCount()
+        {
+            Dictionary<string, CensusDTO> indiaStateCodeTotalRecords = new Dictionary<string, CensusDTO>();
 
-            Assert.AreEqual(29,totalRecords.Count);
-            Assert.AreEqual(37, stateRecords.Count);
+            indiaStateCodeTotalRecords = censusAnalyzer.LoadCensusData(Country.INDIA, indianStateCodeFilePath, indianStateCodeHeaders);
+
+            Assert.AreEqual(37, indiaStateCodeTotalRecords.Count);
+        }
+
+        [Test]
+        public void GivenIncorrectIndianCensusDataFile_WhenRead_ShouldReturnException()
+        {
+            try
+            {
+                Dictionary<string, CensusDTO> indiaStateCensusTotalRecords = new Dictionary<string, CensusDTO>();
+
+                indiaStateCensusTotalRecords = censusAnalyzer.LoadCensusData(Country.INDIA, wrongIndianStateCensusFilePath, indianStateCensusHeaders);
+                
+            }
+            catch(CensusAnalyzerException e)
+            {
+                Assert.AreEqual(CensusAnalyzerException.ExceptionType.FILE_NOT_FOUND, e.eType);
+            }
+            
+        }
+
+        [Test]
+        public void GivenIncorrectIndianStateCodeFile_WhenRead_ShouldReturnException()
+        {
+            try
+            {
+                Dictionary<string, CensusDTO> indiaStateCodeTotalRecords = new Dictionary<string, CensusDTO>();
+
+                indiaStateCodeTotalRecords = censusAnalyzer.LoadCensusData(Country.INDIA, wrongIndianStateCodeFilePath, indianStateCodeHeaders);
+
+            }
+            catch (CensusAnalyzerException e)
+            {
+                Assert.AreEqual(CensusAnalyzerException.ExceptionType.FILE_NOT_FOUND, e.eType);
+            }
+
+        }
+
+        [Test]
+        public void GivenIncorrectIndianCensusDataFileType_WhenRead_ShouldReturnException()
+        {
+            try
+            {
+                Dictionary<string, CensusDTO> indiaStateCensusTotalRecords = new Dictionary<string, CensusDTO>();
+
+                indiaStateCensusTotalRecords = censusAnalyzer.LoadCensusData(Country.INDIA, wrongIndianStateCensusFileType, indianStateCensusHeaders);
+
+            }
+            catch (CensusAnalyzerException e)
+            {
+                Assert.AreEqual(CensusAnalyzerException.ExceptionType.INVALID_FILE_TYPE, e.eType);
+            }
+
+        }
+
+        [Test]
+        public void GivenIncorrectIndianStateCodeFileType_WhenRead_ShouldReturnException()
+        {
+            try
+            {
+                Dictionary<string, CensusDTO> indiaStateCodeTotalRecords = new Dictionary<string, CensusDTO>();
+
+                indiaStateCodeTotalRecords = censusAnalyzer.LoadCensusData(Country.INDIA, wrongIndianStateCodeFileType, indianStateCodeHeaders);
+
+            }
+            catch (CensusAnalyzerException e)
+            {
+                Assert.AreEqual(CensusAnalyzerException.ExceptionType.INVALID_FILE_TYPE, e.eType);
+            }
+
+        }
+        [Test]
+        public void GivenIncorrectIndianCensusDataDelimiter_WhenRead_ShouldReturnException()
+        {
+            try
+            {
+                Dictionary<string, CensusDTO> indiaStateCensusTotalRecords = new Dictionary<string, CensusDTO>();
+
+                indiaStateCensusTotalRecords = censusAnalyzer.LoadCensusData(Country.INDIA, delimiterIndianCensusFilePath, indianStateCensusHeaders);
+
+            }
+            catch (CensusAnalyzerException e)
+            {
+                Assert.AreEqual(CensusAnalyzerException.ExceptionType.INCORRECT_DELIMITER, e.eType);
+            }
+
+        }
+
+        [Test]
+        public void GivenIncorrectIndianStateCodeDelimiter_WhenRead_ShouldReturnException()
+        {
+            try
+            {
+                Dictionary<string, CensusDTO> indiaStateCodeTotalRecords = new Dictionary<string, CensusDTO>();
+
+                indiaStateCodeTotalRecords = censusAnalyzer.LoadCensusData(Country.INDIA, delimiterIndianStateCodeFilePath, indianStateCodeHeaders);
+
+            }
+            catch (CensusAnalyzerException e)
+            {
+                Assert.AreEqual(CensusAnalyzerException.ExceptionType.INCORRECT_DELIMITER, e.eType);
+            }
+
+        }
+
+        [Test]
+        public void GivenIncorrectHeaderForIndianCensusData_WhenRead_ShouldReturnException()
+        {
+            try
+            {
+                Dictionary<string, CensusDTO> indiaStateCensusTotalRecords = new Dictionary<string, CensusDTO>();
+
+                indiaStateCensusTotalRecords = censusAnalyzer.LoadCensusData(Country.INDIA, wrongHeaderIndianCensusFilePath, indianStateCensusHeaders);
+
+            }
+            catch (CensusAnalyzerException e)
+            {
+                Assert.AreEqual(CensusAnalyzerException.ExceptionType.INCORRECT_HEADER, e.eType);
+            }
+
+        }
+
+        [Test]
+        public void GivenIncorrectHeaderIndianStateCode_WhenRead_ShouldReturnException()
+        {
+            try
+            {
+                Dictionary<string, CensusDTO> indiaStateCodeTotalRecords = new Dictionary<string, CensusDTO>();
+
+                indiaStateCodeTotalRecords = censusAnalyzer.LoadCensusData(Country.INDIA, wrongHeaderStateCodeFilePath, indianStateCodeHeaders);
+
+            }
+            catch (CensusAnalyzerException e)
+            {
+                Assert.AreEqual(CensusAnalyzerException.ExceptionType.INCORRECT_HEADER, e.eType);
+            }
+
         }
     }
 }
