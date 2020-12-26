98a119aa7d37903ff481028ba3f184a7eac2b4c6
Anuj Sharma <intrstelr.007@gmail.com>
2020-12-26 17:15:56 +0530

    [Anuj] Refactor. Made changes to Adapter and Analyzer class

--- IndianStateCensusAnalyzer/POCO/CensusAnalyzerException.cs
+++ IndianStateCensusAnalyzer/POCO/CensusAnalyzerException.cs
@@ -6,11 +6,19 @@ namespace IndianStateCensusAnalyzer.POCO
 {
     public class CensusAnalyzerException:Exception
     {
+        /// <summary>
+        /// Enum for Exception types
+        /// </summary>
         public enum ExceptionType
         {
             FILE_NOT_FOUND,INVALID_FILE_TYPE,INCORRECT_DELIMITER,INCORRECT_HEADER,NO_SUCH_ARRAY,NO_SUCH_COUNTRY
         }
         public ExceptionType eType;
+        /// <summary>
+        /// Initializes a new instance of the <see cref="CensusAnalyzerException"/> class.
+        /// </summary>
+        /// <param name="message">The message.</param>
+        /// <param name="exceptionType">Type of the exception.</param>
         public CensusAnalyzerException(string message, ExceptionType exceptionType) : base(message)
         {
             this.eType = exceptionType;
